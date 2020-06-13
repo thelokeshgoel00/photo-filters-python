e87ad2d18e6811cee3b2b680f5f687050d361b88
hardik2020 <44315408+hardik2020@users.noreply.github.com>
2020-06-13 16:55:10 +0530

    Add files via upload

--- gui_frontend.py
+++ gui_frontend.py
@@ -1,7 +1,7 @@
 from tkinter import *
 from tkinter import filedialog
 from PIL import Image,ImageTk
-
+root = Tk()
 
 class MainWindow():
 
@@ -10,36 +10,41 @@ class MainWindow():
     def __init__(self, main):
 
         # canvas for image
-        main['width']=800
-        main['height']=800
-        self.canvas = Canvas(main, width=300, height=300)
-        self.canvas.grid(row=0, column=0)
+        main['width']=600
+        main['height']=600
 
-        self.img = PhotoImage(file="tau.png")
+        im = Image.open("C:/Users/user/Downloads/tau.png")
 
-        # set first image on canvas
-        self.image_on_canvas = self.canvas.create_image(0, 0, anchor = NW, image = self.img)
+        im = im.resize((200, 200))
+        tkimage = ImageTk.PhotoImage(im)
+        self.myvar = Label(main,image=tkimage)
+        self.myvar.image = tkimage
+        self.myvar.grid(row=1,column=1)
 
         # button to change image
         self.button = Button(main, text="Change", command=self.onButton)
-        self.button.grid(row=1, column=0)
+        self.button.grid(row=3,column=1)
+
 
 
 
     def onButton(self):
 
         # next image
-        filename = filedialog.askopenfilename(initialdir="/", title="Select file",
+        path = filedialog.askopenfilename(initialdir="/", title="Select file",
                                               filetypes=(("jpeg files", "*.jpg"), ("png files", "*.png"),
                                                          ("jpeg files", "*.jpeg")))
-        filename = "hello.jpg"
-        img = ImageTk.PhotoImage(file=filename)
+        im = Image.open(path)
+        #im.show()
+        im=im.resize((200,200))
+        tkimage = ImageTk.PhotoImage(im)
+        self.myvar.configure(image=tkimage)
+        self.myvar.image = tkimage
+        self.myvar.grid(row=1, column=1)
 
-        # change image
-        self.canvas.itemconfig(self.image_on_canvas, image = img)
 
 
 
-root = Tk()
+
 MainWindow(root)
 root.mainloop()
